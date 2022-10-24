d3b7d34bd83e4ee48b7dd1d8690f3b16c15288ea
unknown <ogabr2020@gmail.com>
2022-10-24 13:55:54 -0500

    Changed and made code cleaner :)

--- src/weather.js
+++ src/weather.js
@@ -13,17 +13,14 @@ const wind = document.getElementById('wind');
 const humidity = document.getElementById('humidity');
 const error = document.querySelector('.error');
 
-
 form_input.addEventListener('keypress', (e) =>{
     if (e.key === "Enter"){
         e.preventDefault();
         console.log('Form submitted!')
         let city_data = e.target.value
-        pingWeatherAPI(city_data).catch((e) => console.log(e));
+        pingWeatherAPI(city_data).catch((e) => render_error());
     }
 })
-
-
 
 function fades(){
     if (city_parent.classList.contains("fade-in2") && temperature_container.classList.contains("fade-in2") && details_container.classList.contains("fade-in2")){
@@ -72,18 +69,14 @@ function reset(){
     return;
 }
 
-function check_if_valid_request(status){
-    let response_status = status;
-    console.log('Display error with response status: ', response_status);
-    if (response_status === 404 || response_status === 400){
-        error.style.visibility = "visible";
-        return false;
-    }else{
-        error.style.visibility = "hidden";
-        
-    }
-    return true;
+function render_error(){
+    return error.style.visibility = "visible";
 }
+
+function render_success(){
+    return error.style.visibility = "hidden";
+}
+
 
 function render_weather_request(data){
     render_city_to_page(data);
@@ -94,21 +87,16 @@ function render_weather_request(data){
 }
 
 async function pingWeatherAPI(city){
-    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}&units=imperial`);
-    let status_code = response.status
-
-    if (check_if_valid_request(status_code) === false){
-        return;
-    }
-    let data = await response.json();
-    return render_weather_request(data);
-
-
-
-
-
+        let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}&units=imperial`);
+        let status_code = response.status
+        if (status_code === 400 || status_code === 404){
+            throw new Error('Response invalid!')
+        }
+        let data = await response.json()
+        render_success()
+        return render_weather_request(data)
+        
 }
 
-// pingWeatherAPI('San Diego').catch((e) => console.log(e));
 
    
